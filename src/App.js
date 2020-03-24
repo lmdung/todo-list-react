9f4e41cb48f9f8cefa4bd14c5829d76b494d1d9f
ledung <lmdung94@gmail.com>
2020-03-25 00:22:20 +0700

    create new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.listItem = [
+      "Dung dep trai",
+      "Dung nha giau",
+      "Dung hoc gioi"
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.listItem.map((item, index) => <TodoItem key={index} title={item}/>)}
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
